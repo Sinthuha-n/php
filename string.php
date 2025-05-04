30cb255c63806ab74ce3964de17bc71c821236b6
Sinthuha Nadesan <nadasinthu09@gmail.com>
2025-05-04 14:44:16 +0530

    string constraint

---
+++ string.php
@@ -0,0 +1,14 @@
+<?php
+
+$firstName = "Sinthuha";
+$lastName = "Nadesan";
+$fullName = $firstName . " " . $lastName;
+echo "Full name: " . $fullName;
+
+echo "<br><br>";
+
+$product = "Book";
+$price = "500";
+echo "The Price of" . $product ." is Rs. " .$price;
+
+?>
